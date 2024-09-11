0f325aa495a91450676b8ca5f20ad43c6fc783f6
mineai <mineai01@gmail.com>
2024-09-11 23:00:42 +0700

    add ft

--- libft/libft.h
+++ libft/libft.h
@@ -1,7 +1,7 @@
-#infdef LIBFT_H
+#ifndef LIBFT_H
 # define LIBFT_H
 
-#include <string.h>
+// #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -10,5 +10,9 @@ typedef unsigned int t_int;
 
 int ft_isalpha(int c);
 int ft_isdigit(int c);
+int ft_isascii(int c);
+int ft_isalnum(int c);
+int ft_isprint(int c);
+size_t ft_strlen(const char *s);
 
 #endif
